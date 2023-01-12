8a7485a01eb9a3cbdbbb866be0d41dc76978f324
Łukasz Burnos <burnoslukasz@gmail.com>
2023-01-12 15:56:16 +0100

    zadanie 5.1 ver_1

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -3,9 +3,32 @@ package pl.javastart.task;
 import java.util.Scanner;
 
 public class CoordinateSystem {
-
-    // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
-        System.out.println("Punkt (5, -3)");
+        System.out.println("Podaj X");
+        int x = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Podaj Y");
+        int y = scanner.nextInt();
+        scanner.nextLine();
+        if (x > 0 && y > 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w I ćwiartce układu współrzędnych");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w II ćwiartce układu współrzędnych");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w III ćwiartce układu współrzędnych");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w IV ćwiartce układu współrzędnych");
+        } else if (x == 0 && y < 0 || y > 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w na osi Y układu współrzędnych");
+        } else if (x > 0 || x < 0 && y == 0) {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży w na osi X układu współrzędnych");
+        } else {
+            System.out.println("Punkt " + "(" + x + ", " + y + ")" + " leży na srodku układu współrzędnych");
+
+        }
     }
+
 }
+
+
+
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Sprawdz w której ćwiartce układu współrzędnych leży punkt (X, Y)");
         CoordinateSystem coordinateSystem = new CoordinateSystem();
         Scanner scanner = new Scanner(System.in);
         coordinateSystem.run(scanner);
